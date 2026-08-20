b17921b0ec8d4a38101ce069269167b38ce337e5
Shamsan-Nasir <shamsannsr@gmail.com>
2026-08-20 16:41:03 +0600

    js function progress

--- utilities.js
+++ utilities.js
@@ -1,23 +1,106 @@
 
 // copy card information 
 
-function copyCardInformationById(id){
-
-
-
-    return card.innerText
-}
 
 // setting event listener and button copy function
 
 function copyBtnSetup(buttonId,infoId){
     
-    document.getElementById(buttonId).setEventListener('click', () => {
+    document.getElementById(buttonId).addEventListener('click', () => {
 
         const card = document.getElementById(infoId)
 
         navigator.clipboard.writeText(card.innerText)
 
+  
+        const copyNo = document.getElementById("copy-no");
+        let copyNumber = parseInt(copyNo.innerText)
+        copyNumber += 1;
+
+        copyNo.innerText = copyNumber;
+
+        window.alert(`copied number ${card.innerText}`)
+
     })
 
 }
+
+function callBtnSetup(buttonId,numberId,descId){
+    
+    document.getElementById(buttonId).addEventListener('click', () => {
+
+        const num = document.getElementById(numberId);
+        const desc = document.getElementById(descId);
+
+  
+        const coinNo = document.getElementById("coin-no");
+        let coinNumber = parseInt(coinNo.innerText)
+        if (coinNumber >= 20 ) {
+            coinNumber -= 20;
+            coinNo.innerText = coinNumber;
+            window.alert(`Calling Number for ${desc.innerText}: ${num.innerText}`)        
+        }
+        else{
+            window.alert("Not Enough Coins for the call")
+        }
+
+    })
+
+}
+
+
+function heartSetUp (heartClicked, heartUnClicked, heartBorder) {
+    
+    let clicked = document.getElementById(heartClicked);
+    let unClicked = document.getElementById(heartUnClicked);
+    let heart = document.getElementById(heartBorder);
+
+    heart.addEventListener('click', () =>{
+        let heartCount = document.getElementById("heart-no")
+        let number = parseInt(heartCount.innerText)
+        if (clicked.style.display === 'none'){
+            clicked.style.display = "block"
+            number += 1;
+            unClicked.style.display = "none"
+        } 
+        else {
+            clicked.style.display = "none"
+            number -= 1;
+            unClicked.style.display = "block"
+
+        }
+
+        heartCount.innerText = number;
+    })
+    
+}
+heartSetUp('clickEmergency','unclickEmergency', "heart-emergency")
+heartSetUp('clickpolice','unclickpolice', "heart-police")
+heartSetUp('clickfire','unclickfire', "heart-fire")
+heartSetUp('clickwomen','unclickwomen', "heart-women")
+heartSetUp('clickambulance','unclickambulance', "heart-ambulance")
+heartSetUp('clickcorruption','unclickcorruption', "heart-corruption")
+heartSetUp('clickelectricity','unclickelectricity', "heart-electricity")
+heartSetUp('clickbrac','unclickbrac', "heart-brac")
+heartSetUp('clickrailway','unclickrailway', "heart-railway")
+
+copyBtnSetup('emergency-copy','emergency-no');
+copyBtnSetup('police-copy','police');
+copyBtnSetup('fire-copy','fire');
+copyBtnSetup('women-copy','women');
+copyBtnSetup('ambulance-copy','ambulance');
+copyBtnSetup('corruption-copy','corruption');
+copyBtnSetup('electricity-copy','electricity');
+copyBtnSetup('brac-copy','brac');
+copyBtnSetup('railway-copy','railway');
+
+callBtnSetup('emergency-call','emergency-no','emergency-desc');
+callBtnSetup('police-call','police','policeDesc');
+callBtnSetup('fire-call','fire','fireDesc');
+callBtnSetup('women-call','women','womenDesc');
+callBtnSetup('ambulance-call','ambulance','ambulanceDesc');
+callBtnSetup('corruption-call','corruption','corruptionDesc');
+callBtnSetup('electricity-call','electricity','electricityDesc');
+callBtnSetup('brac-call','brac','bracDesc');
+callBtnSetup('railway-call','railway','railwayDesc');
+ 
